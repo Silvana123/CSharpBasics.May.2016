989b870e99e91eae649bb1f402c719c6fb6036ae
Silvana Matova <smatova@gmail.com>
2016-05-13 13:37:31 +0300

    https://github.com/Silvana123/CSharpBasics.May.2016.git

--- 01.FirstStepsInCSharp/05.Num1To20/Properties/AssemblyInfo.cs
+++ 01.FirstStepsInCSharp/05.Num1To20/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("5.Nums1To20")]
+[assembly: AssemblyTitle("05.Num1To20")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("5.Nums1To20")]
+[assembly: AssemblyProduct("05.Num1To20")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31065dfa-fc7c-4e8b-b242-4473058cad2f")]
+[assembly: Guid("102a2040-3412-42cc-a3bc-9379d078a464")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ 01.FirstStepsInCSharp/08.SquareOfTheNStars/08.RectangleOfTheNStars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class PrintSquareStars
+
+{
+    static void Main()
+
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(new string('*', n));
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.Write("*");
+            Console.Write(new string(' ', n - 2));
+            Console.Write("*");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(new string('*', n));
+
+    }
+
+}
+
+
+
